dc0ded6e3059a83976cd006f89e4c40afc53b598
iKadmium <kadmium@gmail.com>
2016-05-25 12:04:34 +1000

    Added basic functionality

---
+++ OSCSend/Options.cs
@@ -0,0 +1,33 @@
+﻿using CommandLineParser.Arguments;
+using CommandLineParser.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSCSend
+{
+    [ArgumentGroupCertification("s,f", EArgumentGroupCondition.ExactlyOneUsed)]
+    class Options
+    {
+        [ValueArgument(typeof(string), 'h', "host", Description = "Set the host", DefaultValue = "localhost")]
+        public string Host { get; set; }
+
+        [ValueArgument(typeof(int), 'p', "port", Description = "Set the port", DefaultValue = 8000)]
+        public int Port { get; set; }
+
+        [ValueArgument(typeof(int?), 'l', "localport", Description = "Set the local port to listen on for a reply", DefaultValue = null)]
+        public int? LocalPort { get; set; }
+
+        [ValueArgument(typeof(string), 'a', "address", Description = "Set the address", Optional = false)]
+        public string Address { get; set; }
+
+        [ValueArgument(typeof(string), 's', "stringValue", Description = "Set the string value")]
+        public string StringValue { get; set; }
+
+        [ValueArgument(typeof(float), 'f', "floatValue", Description = "Set the float value")]
+        public float FloatValue { get; set; }
+        
+    }
+}
---
+++ OSCSend/Program.cs
@@ -0,0 +1,74 @@
+﻿using CommandLineParser.Arguments;
+using Rug.Osc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OSCSend
+{
+    class Program
+    {
+        static TimeSpan timeoutTime = new TimeSpan(0, 0, 5);
+        static void Main(string[] args)
+        {
+            CommandLineParser.CommandLineParser parser = new CommandLineParser.CommandLineParser();
+            parser.ShowUsageOnEmptyCommandline = true;
+            parser.AcceptSlash = false;
+            Options options = new Options();
+            parser.ExtractArgumentAttributes(options);
+            parser.ParseCommandLine(args);
+
+            UdpClient client = new UdpClient();
+
+            Console.WriteLine("Sending to " + options.Address);
+
+            object argument = options.StringValue == null ? (object)options.FloatValue : options.StringValue;
+            OscMessage message = new OscMessage(options.Address, argument);
+
+            UdpClient server = new UdpClient();
+
+            try
+            {
+                if(options.LocalPort.HasValue)
+                {
+                    server = new UdpClient(options.LocalPort.Value);
+                }
+                client.Send(message.ToByteArray(), message.SizeInBytes, options.Host, options.Port);
+                Console.WriteLine("Sent.");
+                if (options.LocalPort.HasValue)
+                {
+                    DateTime startedTime = DateTime.Now;
+                    Console.Write("Waiting for response");
+                    while(server.Available == 0 && (DateTime.Now - startedTime) < timeoutTime)
+                    {
+                        Thread.Sleep(1000);
+                        Console.Write(".");
+                    }
+                    if(server.Available > 0)
+                    {
+                        IPEndPoint remote = null;
+                        byte[] received = server.Receive(ref remote);
+                        OscPacket reply = OscPacket.Read(received, received.Length);
+                        Console.WriteLine(reply.ToString());
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Timed out");
+                    }
+                }
+            }
+            catch(SocketException e)
+            {
+                Console.Error.WriteLine(e.Message);
+            }
+            
+
+        }
+    }
+}
